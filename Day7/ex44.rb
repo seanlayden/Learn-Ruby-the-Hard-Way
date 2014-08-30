8eea7bfcb0b0b8cbe92156ae0442611d9411b36a
Sean Layden <seanlayden@gmail.com>
2014-08-29 18:10:19 -0600

    Adding files

--- Day7/ex44.rb
+++ Day7/ex44.rb
@@ -15,10 +15,12 @@ class Parent
 end
 
 class Child < Parent
+  # This overrides the parent line, by using the same function name.
   def override()
     puts "CHILD override()"
   end
 
+  # This is an inherited function, and is altered using super().
   def altered()
     puts "CHILD, BEFORE PARENT altered()"
     super()
@@ -37,3 +39,9 @@ son.override()
 
 dad.altered()
 son.altered()
+
+
+# Some rules that MAY be a good idea to follow, should the circumstances be right. Don't be a slave to them.
+# 1. Avoid something called "meta-programming" at all costs, as it is too complex to use reliably.
+# 2. Use composition to package up code into modules that are used in many unrelated places and situations.
+# 3. Use inheritance only when there are clearly related reusable pieces of code that fit under a single common concept or if you have to.
